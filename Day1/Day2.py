17773d3de7bad1aa44508c752bab8b427dacec4c
Shraddhanand <1shraddhanand@gmail.com>
2023-01-21 23:16:19 +0530

    Type Casting

--- Day1/Day2.py
+++ Day1/Day2.py
@@ -3,14 +3,23 @@ a = 4
 b = "sahil"
 c = None
 d = True
-
-# print(a, b, c, d)
+print(a, b, c, d)
 
 # TYPE
-# print("class of c " , type(c))
+print("class of c " , type(c))
 
-# COMPLES
+# COMPLEX
 e = complex(4+ 4j)
 f = complex(4+ 4)
 print("type of e is " ,e, type(e))
 print("type of e is " ,f, type(f))
+
+# TypeCasting
+a = "4"
+b = "sahil with 2"
+c = "2"
+print(a+c)
+
+# two types 
+# 1. Explicit by us or programmers.
+# 2. Implicit by python or programming language. 
