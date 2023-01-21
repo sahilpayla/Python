7b170c5b4e8f7962a5d486118252beca37d99fef
Shraddhanand <1shraddhanand@gmail.com>
2023-01-21 22:04:58 +0530

    multiline comment

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,1 +1,12 @@
+#https://github.com/sahilpayla/Python.git
+# To comment Any Line In The Python We Use -----> "#"
+
 print("hello World")
+
+
+
+'''
+hello world 
+by the way we use this for multiline comment
+bye ....
+'''
