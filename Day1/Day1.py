88910bc11cdd989776ad1ccbf176edb1794b9dc4
Shraddhanand <1shraddhanand@gmail.com>
2023-01-21 22:12:39 +0530

    backslash

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,7 +2,7 @@
 # To comment Any Line In The Python We Use -----> "#"
 
 print("hello World")
-
+print("hi i want to specialize this word \"WORD\" so by using '\n and \' we do something else in the python ")
 
 
 '''
@@ -13,5 +13,9 @@ by the way we use this for multiline comment
 Three times  ---> """ use for same 
 
 """
+
+escape characters 
+1. \n ----> backslash n for using <br> in the python...
+2. 
 bye ....
 '''
