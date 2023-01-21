ff12fec662b0c44ca0b0f01a03156bb9a6385082
Shraddhanand <1shraddhanand@gmail.com>
2023-01-21 23:02:31 +0530

    variables

--- Day1/Day2.py
+++ Day1/Day2.py
@@ -1,6 +1,16 @@
+# VARIABLES && DATATYPES
 a = 4
 b = "sahil"
 c = None
 d = True
 
-print(a, b, c, d)
+# print(a, b, c, d)
+
+# TYPE
+# print("class of c " , type(c))
+
+# COMPLES
+e = complex(4+ 4j)
+f = complex(4+ 4)
+print("type of e is " ,e, type(e))
+print("type of e is " ,f, type(f))
