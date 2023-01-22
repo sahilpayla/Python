c8ea14fe4f6f7017be529308b62a091440533e24
Shraddhanand <1shraddhanand@gmail.com>
2023-01-22 12:27:00 +0530

    string started

---
+++ Day3.py
@@ -0,0 +1,14 @@
+# https://github.com/sahilpayla/Python.git
+
+# INPUT TAKING IN PYTHON
+
+# most basic input taking
+a = input("Enter Your Name: ")
+# print(a)
+
+# type casting in the python input taking
+x = input("Enter number : ")
+y = input("Enter number : ")
+
+print(int(x) + int(y))
+print(float(x) * float(y))
---
+++ Day4.py
@@ -0,0 +1,17 @@
+
+
+# Strings
+
+# this is a way of string in python
+str = '''lorem   
+lorem lorem
+multiline string
+'''
+print(str)
+
+
+
+# for loop in string in python
+print('for loop \n')
+for charaters in str:
+   print(charaters)
